8e3fa5091e1647d15968e58326371e0ddd089834
Niyaz Khan <your_email@example.com>
2025-11-02 13:53:40 +0530

    Completed JP Morgan Virtual Internship Task 1

--- client_test.py
+++ client_test.py
@@ -1,25 +1,25 @@
+#!/usr/bin/env python3
 import unittest
-from client import getDataPoint
+from client import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
-  def test_getDataPoint_calculatePrice(self):
-    quotes = [
-      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
-    """ ------------ Add the assertion below ------------ """
+    def test_getDataPoint_calculatePrice(self):
+        quotes = [
+            {'top_ask': {'price': 121.2, 'size': 36},
+             'top_bid': {'price': 120.48, 'size': 109},
+             'stock': 'ABC'},
+            {'top_ask': {'price': 121.68, 'size': 4},
+             'top_bid': {'price': 117.87, 'size': 81},
+             'stock': 'DEF'}
+        ]
+        for quote in quotes:
+            stock, bid_price, ask_price, price = getDataPoint(quote)
+            self.assertEqual(price, (bid_price + ask_price) / 2)
 
-  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
-    quotes = [
-      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
-    """ ------------ Add the assertion below ------------ """
-
-
-  """ ------------ Add more unit tests ------------ """
-
-
+    def test_getRatio_calculateRatio(self):
+        self.assertEqual(getRatio(10, 2), 5)
+        self.assertEqual(getRatio(2, 10), 0.2)
+        self.assertIsNone(getRatio(10, 0))
 
 if __name__ == '__main__':
     unittest.main()
